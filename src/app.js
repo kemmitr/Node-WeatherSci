82503b74358298064564b1e261f6e6347f3de0e3
kemmitr <russell.kemmit@smail.rasmussen.edu>
2019-05-24 18:22:08 -0500

    made necesarry alteratiosn for heroku.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 const path = require('path');
 const hbs = require('hbs');
-app.listen(3000, () => { console.log('server up in 3000'); });
+app.listen(port, () => { console.log('server up in'+ port); });
 app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', path.join(__dirname, '../templates/views'));
 app.set('view engine', 'hbs');
